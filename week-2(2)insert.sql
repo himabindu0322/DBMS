15ef16fd37377685e5befc01ff03b59a53bcb257
Himabindu <karrihimabindu22@gmail.com>
2025-10-23 10:59:55 +0530

    Create week-2(2)insert.sql

---
+++ week-2(2)insert.sql
@@ -0,0 +1,5 @@
+insert into company values('wipro',5000 ); 
+insert into company values('ibm',8000);
+insert into company values('dell', 9000 );
+insert into company values('wipro',2000 ); 
+insert into company values('dell',10000); 
