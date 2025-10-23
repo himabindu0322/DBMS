d9fa3bf662ece8e70197e455549e0a2fb9e255fb
Himabindu <karrihimabindu22@gmail.com>
2025-10-23 10:51:23 +0530

    Create week-2(1)insert.sql
    Inserting data into students table and displaying the data in the table

---
+++ week-2(1)insert.sql
@@ -0,0 +1,6 @@
+insert into students values('24B11CS234','Bala'); 
+insert into students values('24B11CS381','Kiran');
+---chech data after insertion
+ select * from students; 
+--Retrieve all details of the student with roll number 
+select * from students where rollno='24B11CS234'
