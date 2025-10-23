19060d9558512aa970ac7c96404acaf37064b306
Himabindu <karrihimabindu22@gmail.com>
2025-10-23 11:39:35 +0530

    Create week-3(1)insert.sql
    inserting and displaying the data from two tables

---
+++ week-3(1)insert.sql
@@ -0,0 +1,16 @@
+--insert data into  table one
+insert into tb1 values(503,'suma',40);
+insert into tb1 values(504,'raju',70);
+insert into tb1 values(505,'ramu',45);
+insert into tb1 values(501,'abhi',50);
+insert into tb1 values(502,'rav1',60);
+
+--insert data into  table two
+insert into tb2 values(501,15000);
+insert into tb2 values(502,5000);
+insert into tb2 values(503,10000);
+insert into tb2 values(504,25000);
+
+---display the data
+select * from tb1;
+select * from tb2;
