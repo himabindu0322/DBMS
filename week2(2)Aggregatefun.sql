724f4398aabca05eb38eccdda62de64d9c3693c5
Himabindu <karrihimabindu22@gmail.com>
2025-10-23 11:05:34 +0530

    Create week2(2)Aggregatefun.sql
    Aggregate functions such as avg,sum,count,max,min are used and queries are written using company data

---
+++ week2(2)Aggregatefun.sql
@@ -0,0 +1,10 @@
+---Find the average salary of company 
+Select AVG(salary) from company;
+---- Find the Sum of salaries of company
+ Select SUM(salary) from company; 
+---Find the Maximum amount of company
+Select Max(salary) from company;
+----Find the Minimum amount of company
+ Select Min(salary) from company;
+-----Find the number of rows in a company
+ Select Count(*) from company;
